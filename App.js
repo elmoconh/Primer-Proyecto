7eced83d9aa41fb96b84390e250b6c2b4c55c2f8
elmoconh <helmer.morales1@gmail.com>
2020-05-02 23:04:11 -0400

    intento

---
+++ App.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
+import React from 'react';
+import MyDrawer from './MyDrawer';
+import {View, Text} from 'react-native';
+
+
+export default function App(){
+  return (
+    <MyDrawer/>
+
+  );
+}
---
+++ MyDrawer/index.js
@@ -0,0 +1,39 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen(props) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <TouchableOpacity onPress={()=>props.navigation.navigate('Profile')}>
+       <Text> Perfil </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Pantalla Perfil</Text>
+    </View>
+  );
+}
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
