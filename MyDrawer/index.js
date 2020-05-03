6817b17e8221af1d1692b196f5aa87c450a3b997
elmoconh <helmer.morales1@gmail.com>
2020-05-03 12:56:06 -0400

    Se agrega Drawer al código

--- MyDrawer/index.js
+++ MyDrawer/index.js
@@ -1,16 +1,18 @@
+/* eslint-disable prettier/prettier */
 // In App.js in a new project
 
 import * as React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 function HomeScreen(props) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <TouchableOpacity onPress={()=>props.navigation.navigate('Profile')}>
-       <Text> Perfil </Text>
+      <Text style={{fontSize: 20}}>Home Screen</Text>
+      <TouchableOpacity style={{width: 200, height: 50, backgroundColor: '#ff5204', padding: 10, alignItems: 'center', borderRadius: 5}} onPress={()=>props.navigation.navigate('Profile')}>
+       <Text style={{ fontSize: 20, color: '#fff'}}> Perfil </Text>
       </TouchableOpacity>
     </View>
   );
@@ -23,7 +25,8 @@ function ProfileScreen() {
     </View>
   );
 }
-const Stack = createStackNavigator();
+
+/*const Stack = createStackNavigator();
 
 function App() {
   return (
@@ -32,6 +35,19 @@ function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />        
       </Stack.Navigator>
+    </NavigationContainer>
+  );
+}*/
+
+const Drawer = createDrawerNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
